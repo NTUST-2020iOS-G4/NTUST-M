acb276fcdf4c47b3214052c3cd79cbc290fbde01
Jeffery Ho <chiachun2491@gmail.com>
2020-05-06 15:38:09 +0800

    Initial Commit

---
+++ NTUST-M/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NTUST-M
+//
+//  Created by Jeffery Ho on 2020/5/6.
+//  Copyright © 2020 NTUST-2020iOS-G4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
